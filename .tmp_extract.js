38d012811a21f239d7fc916a5884bffbe1442527
Nabilkarem911 <nabilkarem@gmail.com>
2026-09-02 15:05:08 +0300

    chore: remove temporary extraction script

--- .tmp_extract.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const s = fs.readFileSync('frontend/js/views/public-design-request.js', 'utf8');
-const i = s.indexOf('async function startDesigner()');
-console.log(s.slice(i - 200, i + 100));
